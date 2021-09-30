8afd48dabfddc58a96712dbb90a32c8ff729ebe0
Mosaad <mosaad@jorri.net>
2021-09-30 16:38:55 +0800

    implement our watcher webrtc config and events

---
+++ src/watcher.js
@@ -0,0 +1,60 @@
+let peerConnection;
+//config STUN/TURN servers config
+const config = {
+    iceServers: [
+        {
+            "urls": "stun:stun.l.google.com:19302",
+        },
+    ]
+};
+
+//coonect user using his host
+const socket = io.connect(window.location.origin);
+const video = document.querySelector("video");
+
+//set remote Description when we get offer from broadcaster
+//also set our local description sdp
+socket.on("offer", (id, description) => {
+    peerConnection = new RTCPeerConnection(config);
+    peerConnection
+        .setRemoteDescription(description)
+        .then(() => peerConnection.createAnswer())
+        .then(sdp => peerConnection.setLocalDescription(sdp))
+        .then(() => {
+            //fire and answer event to reply to broadcaster offer
+            socket.emit("answer", id, peerConnection.localDescription);
+        });
+    peerConnection.ontrack = event => {
+        video.srcObject = event.streams[0];
+    };
+    peerConnection.onicecandidate = event => {
+        if (event.candidate) {
+            socket.emit("candidate", id, event.candidate);
+        }
+    };
+});
+
+//when we get ice candidate we add it to our peer
+socket.on("candidate", (id, candidate) => {
+    peerConnection
+        .addIceCandidate(new RTCIceCandidate(candidate))
+        .catch(e => console.error(e));
+});
+
+//fire and watcher event when we get connect event
+
+socket.on("connect", () => {
+    socket.emit("watcher");
+});
+
+//fire and watcher event when we get broadcaster event
+socket.on("broadcaster", () => {
+    socket.emit("watcher");
+});
+
+//close connection when client close window
+window.onunload = window.onbeforeunload = () => {
+    socket.close();
+    peerConnection.close();
+};
+
